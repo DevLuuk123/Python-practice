1ce24b83d1c0496d23c06f8f1058fdbe35699a4d
DevLuuk123 <150522315+DevLuuk123@users.noreply.github.com>
2025-11-03 21:48:14 +0100

    Changelog
    Changelog:
    - Added Calc.py

--- utils/calc.py
+++ utils/calc.py
@@ -1,5 +1,23 @@
 # Made by Luuk Klingens
 # GitHub: https://github.com/devluuk123
-# Copyright Luuk Klingens 2025
-import time
+# Copyright 2025
 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+op = input("Enter operator (+, -, *, /): ")
+
+if op == "+":
+    result = num1 + num2
+elif op == "-":
+    result = num1 - num2
+elif op == "*":
+    result = num1 * num2
+elif op == "/":
+    if num2 == 0:
+        result = "Error: division by zero"
+    else:
+        result = num1 / num2
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
